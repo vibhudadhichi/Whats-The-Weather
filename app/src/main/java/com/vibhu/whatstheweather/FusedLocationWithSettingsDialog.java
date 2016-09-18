95966afd0cb390425a5f65686989f8975f69df34
vibhudadhichi <vibhudadhikhi@gmail.com>
2016-09-18 15:56:39 +0530

    Location Added

--- app/src/main/java/com/vibhu/whatstheweather/FusedLocationWithSettingsDialog.java
+++ app/src/main/java/com/vibhu/whatstheweather/FusedLocationWithSettingsDialog.java
@@ -1,26 +1,17 @@
 package com.vibhu.whatstheweather;
 
+
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -33,129 +24,108 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-
-
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+/**
+ *
+ *
+ * step1 :buildGoogleApiClient
+ * step2: createLocationRequest
+ * step3: buildLocationSettingsRequest
+ *
+ *
+ * step 4:on click of button detect location check the location is on/off using checkLocationSettings method.
+ *
+ * step 5:on click of the options in location dialog
+ *
+ * step 6 :depending on the action taken on dialog (startResolution for result and check the action in onActivityResult
+ *
+ */
+public class FusedLocationWithSettingsDialog extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener,
         ResultCallback<LocationSettingsResult> {
 
-        //Ui widgets
+    //Ui widgets
 
 
-        private String txt_location;
-        protected String mLastUpdateTimeTextView;
-        protected String mLatitudeTextView;
-        protected String mLongitudeTextView;
+    private String txt_location;
+    protected String mLastUpdateTimeTextView;
+    protected String mLatitudeTextView;
+    protected String mLongitudeTextView;
 
-        //New views for city and pincode
-        protected String tv_pincode,tv_city;
-
-
-        /**
-         * Constant used in the location settings dialog.
-         */
-        protected static final int REQUEST_CHECK_SETTINGS = 0x1;
-
-        /**
-         * The desired interval for location updates. Inexact. Updates may be more or less frequent.
-         */
-        public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
-
-        /**
-         * The fastest rate for active location updates. Exact. Updates will never be more frequent
-         * than this value.
-         */
-        public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
-                UPDATE_INTERVAL_IN_MILLISECONDS / 2;
-
-        // Keys for storing activity state in the Bundle.
-        protected final static String KEY_REQUESTING_LOCATION_UPDATES = "requesting-location-updates";
-        protected final static String KEY_LOCATION = "location";
-        protected final static String KEY_LAST_UPDATED_TIME_STRING = "last-updated-time-string";
-
-        /**
-         * Provides the entry point to Google Play services.
-         */
-        protected GoogleApiClient mGoogleApiClient;
-
-        /**
-         * Stores parameters for requests to the FusedLocationProviderApi.
-         */
-        protected LocationRequest mLocationRequest;
-
-        /**
-         * Stores the types of location services the client is interested in using. Used for checking
-         * settings to determine if the device has optimal location settings.
-         */
-        protected LocationSettingsRequest mLocationSettingsRequest;
-
-        /**
-         * Represents a geographical location.
-         */
-        protected Location mCurrentLocation;
+    //New views for city and pincode
+    protected String tv_pincode,tv_city;
 
 
-        // Labels.
-        protected String mLatitudeLabel;
-        protected String mLongitudeLabel;
-        protected String mLastUpdateTimeLabel;
+    protected static final String TAG = "MainActivity";
 
-        /**
-         * Tracks the status of the location updates request. Value changes when the user presses the
-         * Start Updates and Stop Updates buttons.
-         */
-        protected Boolean mRequestingLocationUpdates;
+    /**
+     * Constant used in the location settings dialog.
+     */
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 
-        /**
-         * Time when the location was updated represented as a String.
-         */
-        protected String mLastUpdateTime;
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+    // Keys for storing activity state in the Bundle.
+    protected final static String KEY_REQUESTING_LOCATION_UPDATES = "requesting-location-updates";
+    protected final static String KEY_LOCATION = "location";
+    protected final static String KEY_LAST_UPDATED_TIME_STRING = "last-updated-time-string";
+
+    /**
+     * Provides the entry point to Google Play services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    protected LocationRequest mLocationRequest;
+
+    /**
+     * Stores the types of location services the client is interested in using. Used for checking
+     * settings to determine if the device has optimal location settings.
+     */
+    protected LocationSettingsRequest mLocationSettingsRequest;
+
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mCurrentLocation;
 
 
-        int RQS_GooglePlayServices=0;
+    // Labels.
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected String mLastUpdateTimeLabel;
+
+    /**
+     * Tracks the status of the location updates request. Value changes when the user presses the
+     * Start Updates and Stop Updates buttons.
+     */
+    protected Boolean mRequestingLocationUpdates;
+
+    /**
+     * Time when the location was updated represented as a String.
+     */
+    protected String mLastUpdateTime;
 
 
-        public static final String TAG = MainActivity.class.getSimpleName();
-
-    private CurrentWeather mCurrentWeather;
-
-    @InjectView(R.id.timeLabel)
-    TextView mTimeLabel;
-    @InjectView(R.id.temperatureLabel)
-    TextView mTemperatureLabel;
-    @InjectView(R.id.humidityValue)
-    TextView mHumidityValue;
-    @InjectView(R.id.precipValue)
-    TextView mPrecipValue;
-    @InjectView(R.id.summaryLabel)
-    TextView mSummaryLabel;
-    @InjectView(R.id.iconImageView)
-    ImageView mIconImageView;
-    @InjectView(R.id.refreshImageView)
-    ImageView mRefreshImageView;
-    @InjectView(R.id.progressBar)
-    ProgressBar mProgressBar;
-    @InjectView(R.id.locationLabel)
-    TextView mLocationLabel;
-
+    int RQS_GooglePlayServices=0;
 
     @Override
     protected void onStart() {
@@ -202,24 +172,21 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         mGoogleApiClient.disconnect();
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //btn_location = (Button) findViewById(R.id.btn_detect_fused_location);
 
 
-            ButterKnife.inject(this);
-            YoYo.with(Techniques.FlipInX)
-                    .duration(3500)
-                    .playOn(findViewById(R.id.temperatureLabel));
-            YoYo.with(Techniques.FlipInX)
-                    .duration(3500)
-                    .playOn(findViewById(R.id.iconImageView));
-
-            mProgressBar.setVisibility(View.INVISIBLE);
+        //total six textviews
+//        txt_location = (TextView) findViewById(R.id.txt_location);
+//        mLatitudeTextView = (TextView) findViewById(R.id.latitude_text);
+//        mLongitudeTextView = (TextView) findViewById(R.id.longitude_text);
+//        mLastUpdateTimeTextView = (TextView) findViewById(R.id.last_update_time_text);
+//        tv_city=(TextView)findViewById(R.id.tv_city);
+//        tv_pincode=(TextView)findViewById(R.id.tv_pincode);
 
         // Set labels.
         mLatitudeLabel = "Latitude ";
@@ -244,28 +211,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         checkLocationSettings();
 
-        //Log.i("Latitude",mLatitudeTextView);
-
-        //final double lat = Double.parseDouble(mLatitudeTextView);
-        //final double lng = Double.parseDouble(mLongitudeTextView);
-
-            final double lat = 12.9716;
-            final double lng = 77.5946;
-
-
-            mRefreshImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    getForecast(lat,lng);
-                }
-            });
+        Log.i("Latitude",mLatitudeTextView);
 
 
 
-            getForecast(lat, lng);
-
-            Log.d(TAG, "Main UI code is running!");
-        }
+    }
 
     //step 1
     protected synchronized void buildGoogleApiClient() {
@@ -315,143 +265,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
 
-
-
-
-    private void getForecast(double latitude, double longitude) {
-
-
-        String apiKey = "27974c4bc33201748eaf542a6769c3b7";
-        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
-                "/" + latitude + "," + longitude;
-
-        if (isNetworkAvailable()) {
-            toggleRefresh();
-
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(forecastUrl)
-                    .build();
-
-            Call call = client.newCall(request);
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(Request request, IOException e) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            toggleRefresh();
-                        }
-                    });
-                    alertUserAboutError();
-                }
-
-                @Override
-                public void onResponse(Response response) throws IOException {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            toggleRefresh();
-                        }
-                    });
-
-                    try {
-                        String jsonData = response.body().string();
-                        Log.v(TAG, jsonData);
-                        if (response.isSuccessful()) {
-                            mCurrentWeather = getCurrentDetails(jsonData);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    updateDisplay();
-                                }
-                            });
-                        } else {
-                            alertUserAboutError();
-                        }
-                    }
-                    catch (IOException e) {
-                        Log.e(TAG, "Exception caught: ", e);
-                    }
-                    catch (JSONException e) {
-                        Log.e(TAG, "Exception caught: ", e);
-                    }
-                }
-            });
-        }
-        else {
-            Toast.makeText(this, getString(R.string.network_unavailable_message),
-                    Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void toggleRefresh() {
-        if (mProgressBar.getVisibility() == View.INVISIBLE) {
-            mProgressBar.setVisibility(View.VISIBLE);
-            mRefreshImageView.setVisibility(View.INVISIBLE);
-        }
-        else {
-            mProgressBar.setVisibility(View.INVISIBLE);
-            mRefreshImageView.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private void updateDisplay() {
-        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
-        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it will be");
-        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
-        mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
-        mSummaryLabel.setText(mCurrentWeather.getSummary());
-        YoYo.with(Techniques.FlipInX)
-                .duration(3500)
-                .playOn(findViewById(R.id.temperatureLabel));
-        YoYo.with(Techniques.FlipInX)
-                .duration(3500)
-                .playOn(findViewById(R.id.iconImageView));
-
-        Drawable drawable = getResources().getDrawable(mCurrentWeather.getIconId());
-        mIconImageView.setImageDrawable(drawable);
-    }
-
-    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
-        JSONObject forecast = new JSONObject(jsonData);
-        String timezone = forecast.getString("timezone");
-        Log.i(TAG, "From JSON: " + timezone);
-
-        JSONObject currently = forecast.getJSONObject("currently");
-
-        CurrentWeather currentWeather = new CurrentWeather();
-        currentWeather.setHumidity(currently.getDouble("humidity"));
-        currentWeather.setTime(currently.getLong("time"));
-        currentWeather.setIcon(currently.getString("icon"));
-        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
-        currentWeather.setSummary(currently.getString("summary"));
-        currentWeather.setTemperature(currently.getDouble("temperature"));
-        currentWeather.setTimeZone(timezone);
-
-        Log.d(TAG, currentWeather.getFormattedTime());
-
-        return currentWeather;
-    }
-
-
-    private boolean isNetworkAvailable() {
-        ConnectivityManager manager = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
-        boolean isAvailable = false;
-        if (networkInfo != null && networkInfo.isConnected()) {
-            isAvailable = true;
-        }
-
-        return isAvailable;
-    }
-
-    private void alertUserAboutError() {
-        AlertDialogFragment dialog = new AlertDialogFragment();
-        dialog.show(getFragmentManager(), "error_dialog");
-    }
-
     /**
      * Requests location updates from the FusedLocationApi.
      */
@@ -489,6 +302,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         });
     }
 
+
+
     @Override
     public void onConnected(Bundle bundle) throws SecurityException {
 
@@ -522,8 +337,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         mCurrentLocation = location;
         mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
         updateLocationUI();
-//        Toast.makeText(this, "Location Updated",
-//                Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Location Updated",
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -549,7 +364,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             case LocationSettingsStatusCodes.SUCCESS:
                 Log.i(TAG, "All location settings are satisfied.");
 
-//                Toast.makeText(this, "Location is already on.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(FusedLocationWithSettingsDialog.this, "Location is already on.", Toast.LENGTH_SHORT).show();
                 startLocationUpdates();
                 break;
             case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
@@ -559,11 +374,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 try {
                     // Show the dialog by calling startResolutionForResult(), and check the result
                     // in onActivityResult().
-//                    Toast.makeText(this, "Location dialog will be open", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FusedLocationWithSettingsDialog.this, "Location dialog will be open", Toast.LENGTH_SHORT).show();
                     //
 
                     //move to step 6 in onActivityResult to check what action user has taken on settings dialog
-                    status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                    status.startResolutionForResult(FusedLocationWithSettingsDialog.this, REQUEST_CHECK_SETTINGS);
                 } catch (IntentSender.SendIntentException e) {
                     Log.i(TAG, "PendingIntent unable to execute request.");
                 }
@@ -600,6 +415,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         }
     }
 
+
+
+
+
     /**
      * Sets the value of the UI fields for the location latitude, longitude and last update time.
      */
@@ -628,12 +447,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     {
         try
         {
-            Geocoder gcd = new Geocoder(MainActivity.this, Locale.getDefault());
+            Geocoder gcd = new Geocoder(FusedLocationWithSettingsDialog.this, Locale.getDefault());
             List<Address> addresses = gcd.getFromLocation(latitude, longitude, 1);
             if (addresses.size() > 0)
             {
 
-                mLocationLabel.setText(addresses.get(0).getLocality());
+                tv_city="City="+addresses.get(0).getLocality();
                 tv_pincode="Pincode="+addresses.get(0).getPostalCode();
                 //  System.out.println(addresses.get(0).getLocality());
             }
@@ -647,20 +466,4 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     }
 
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
